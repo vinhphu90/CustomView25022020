d740b2af13bbad34aaee9d1315935828de36fa4f
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-24 16:10:49 +0700

    custom class view

---
+++ app/src/main/java/com/examplevinhphutvp/customview25022020/EdittextcircleIcon.java
@@ -0,0 +1,19 @@
+package com.examplevinhphutvp.customview25022020;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class EdittextcircleIcon extends RelativeLayout {
+    public EdittextcircleIcon(Context context) {
+        super(context);
+    }
+
+    public EdittextcircleIcon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EdittextcircleIcon(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
