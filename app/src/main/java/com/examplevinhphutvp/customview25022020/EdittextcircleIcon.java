6b7fbacb4b575cb6b41d32bdac05775f30f8827f
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-24 18:47:20 +0700

    finish

--- app/src/main/java/com/examplevinhphutvp/customview25022020/EdittextcircleIcon.java
+++ app/src/main/java/com/examplevinhphutvp/customview25022020/EdittextcircleIcon.java
@@ -1,19 +1,62 @@
 package com.examplevinhphutvp.customview25022020;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class EdittextcircleIcon extends RelativeLayout {
+    private TextView mTvTitle;
+    private ImageView mImgIcon;
+    private String mTextTitle;
+    private int mRedId;
+    private View mView ;
+
+
     public EdittextcircleIcon(Context context) {
         super(context);
+        initLayout(null , null);
     }
 
     public EdittextcircleIcon(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initLayout( context , attrs);
     }
 
     public EdittextcircleIcon(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+    private void initLayout(@Nullable Context context, AttributeSet attrs) {
+        if (context != null && attrs != null){
+            initLayout(context);
+            TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.EdittextcircleIcon);
+            mTextTitle = typedArray.getString(R.styleable.EdittextcircleIcon_title);
+            mRedId = typedArray.getResourceId(R.styleable.EdittextcircleIcon_icon_left,0);
+            typedArray.recycle();
+            mapview();
+            setupView();
+        }
+    }
+
+
+    private void initLayout(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context. getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        mView= inflater.inflate(R.layout.layout_edittext_red_circle,this,true);
+    }
+
+    private void mapview() {
+        mTvTitle= mView. findViewById(R.id.tvtTitle);
+        mImgIcon= mView. findViewById(R.id.imgIcon);
+    }
+
+    private void setupView() {
+        mTvTitle.setText(mTextTitle);
+        mImgIcon.setImageResource(mRedId);
     }
 }
